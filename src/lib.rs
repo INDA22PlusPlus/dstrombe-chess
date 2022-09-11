c19bd1d49240247ac24db16dd8421f9f03a415c4
c14 <danielrrginting@gmail.com>
2022-09-11 17:00:10 +0200

    implemented QoL struct Pos + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,24 @@
-use tokio;
-
+mod util;
+#[derive(Copy, Clone)]
 enum Color{
     White,
     Black
 }
 
-struct Board {
-    board : Vec<Vec<Box<dyn Piece>>>
+
+struct PGN_tracker {
+	
+}
+struct FEN {
+	
 }
 
-trait Piece {
-    async fn is_legal_move(&self, from_x : i8, from_y : i8, to_x : i8, to_y : i8) -> bool;
-    fn color(&self) -> Color;
-}
 
-struct Pawn {
-    async fn is_legal_move(&self, board :  Board, from_x : i8, from_y : i8, to_x : i8, to_y : i8) -> bool {
-        dir : bool = match self.color() {
-            Color.White() => 1,
-            Color.Black() => -1
-        };
-        let piece_at_square =piece_at_square = 
-    }
 
-    pub did_doublemove : bool,
-}
 
-struct King {
 
-}
 
-struct Queen {
 
-}
 
-struct Rook {
 
-}
-
-struct Bishop {
-
-}
-
-impl Piece for Pawn {
-    async fn is_legal_move(&self, from)
-}
 
